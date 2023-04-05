8f1c64e71afbe316c0cba0f3577499cb9e46d633
aditya <sandesh.more821@gmail.com>
2023-04-05 15:16:53 +0530

    added

---
+++ main.tf
@@ -0,0 +1,61 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.82.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+provider "azurerm" {
+  features {}
+}
+
+
+
+
+
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "aazure-resorce-group-RTL"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageforlogicappterraform"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "azure-functions-test-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "elastic"
+
+
+  sku {
+    tier = "WorkflowStandard"
+    size = "WS1"
+  }
+}
+
+resource "azurerm_logic_app_standard" "example" {
+  name                       = "terraform-v1-logic-app"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"     = "node"
+    "WEBSITE_NODE_DEFAULT_VERSION" = "~18"
+  }
+}
+
